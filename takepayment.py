e28116a76677ef20257414414a1612a5ca737809
Anshika Kumari <ak0379@srmist.edu.in>
2026-03-13 10:36:28 +0530

    QR Code generator

---
+++ takepayment.py
@@ -0,0 +1,37 @@
+import qrcode
+
+#taking UPI ID as a input
+upi_id = input("Enter your UPI ID = ")
+
+#upi://pay?pa=UPI_ID&apn=Name&am=Amount&cu=CURRENCY&tn=MESSAGE
+#pa, pn, am , cu, tn all these are the parameters of the UPI QR code
+#pa = payee address (UPI Id)
+#pn = payee Name
+#am=amount
+#cu = currency
+#tn=transaction note
+#mc= mechant code
+
+#payment URL based on the UPI ID and the Payment app
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+#generating QR code for Phonepe
+phonepe_qr = qrcode.make(phonepe_url)
+phonepe_qr.save("phonepe_qr.png")
+
+#generating QR code for Paytm
+paytm_qr=qrcode.make(paytm_url)
+paytm_qr.save("paytm.png")
+
+#generating QR code for Google Pay
+google_pay_qr=qrcode.make(google_pay_url)
+google_pay_qr.save("google_pay.png")
+
+
+#to display the QR codes, you can use an image viewer or open the generated PNG files, for that yoy need to install PIL/Pillow library
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
+
